c300ce85a4e81290a379fedd20d3989ed7dbbf31
James Robb <47126579+jamesrweb@users.noreply.github.com>
2022-10-25 04:26:16 +0200

    Fix minification issue for non-optimised builds

--- apps/client/build.js
+++ apps/client/build.js
@@ -1,43 +1,46 @@
 import { minify } from 'uglify-js'
 
+const minifyOptions = {
+  compress: {
+    pure_funcs: [
+      'F2',
+      'F3',
+      'F4',
+      'F5',
+      'F6',
+      'F7',
+      'F8',
+      'F9',
+      'A2',
+      'A3',
+      'A4',
+      'A5',
+      'A6',
+      'A7',
+      'A8',
+      'A9',
+    ],
+    pure_getters: true,
+    keep_fargs: false,
+    unsafe_comps: true,
+    unsafe: true,
+  },
+  mangle: {
+    reserved: ['Elm'],
+  },
+}
+
 /** @type {import("elm-watch/elm-watch-node").Postprocess} */
 export default function postprocess(options) {
-  if (options.compilationMode === 'debug') {
-    return
-  }
-
-  if (options.compilationMode === 'standard') {
-    return
+  if (
+    options.runMode === 'hot' ||
+    ['debug', 'standard'].includes(options.compilationMode)
+  ) {
+    return options.code
   }
 
   if (options.runMode === 'make' && options.compilationMode === 'optimize') {
-    const result = minify(options.code, {
-      compress: {
-        pure_funcs: [
-          'F2',
-          'F3',
-          'F4',
-          'F5',
-          'F6',
-          'F7',
-          'F8',
-          'F9',
-          'A2',
-          'A3',
-          'A4',
-          'A5',
-          'A6',
-          'A7',
-          'A8',
-          'A9',
-        ],
-        pure_getters: true,
-        keep_fargs: false,
-        unsafe_comps: true,
-        unsafe: true,
-      },
-      mangle: true,
-    })
+    const result = minify(options.code, minifyOptions)
 
     if (result.error !== undefined) {
       throw new Error(error)
